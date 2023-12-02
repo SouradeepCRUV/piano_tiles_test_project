587e675139011a0beea7587afe0e9eb17680b688
Souradeep Banerjee <rishibanerjee1101@gmail.com>
2023-12-02 18:09:48 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/piano_tiles_test_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.piano_tiles_test_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/line_divider.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class LineDivider extends StatelessWidget {
+  const LineDivider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: 1,
+      color: Colors.white,
+    );
+  }
+}
